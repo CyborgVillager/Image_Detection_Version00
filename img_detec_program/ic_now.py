6b232d95bd6fb27fede5a1da3180a14c9ad56ffa
CyborgVillager <almawijonathan@gmail.com>
2020-02-28 03:40:49 -0600

    v5 Try/Except Custom File
    Try/Except Custom File: User can now type in the file they want the computer to scan, and read the info
                                afterwords the completed images will be sent to folder image_detection_complete with the format
                                of imgname.jpg_img_complete to signify to the user the computer has finished scanning the
                                photo for data & is ready to be seen by a human to see the image probability.

--- img_detec_program/ic_now.py
+++ img_detec_program/ic_now.py
@@ -1,16 +1,18 @@
+import glob
+
 try:
     from colors import *
+
     print(yellow + 'Import Colors successful!' + end)
-except:
+except ImportError:
     print('Unable to import Colors from source/colors.py')
 
 try:
     from ic_now_source import *
+
     print(yellow + 'Source import successful can begin operation for Excecution Path' + end)
-except:
-    print('Unable to aquire ic_now_source import')
-
-
+except ImportError:
+    print('Unable to import ic_now_source import')
 
 # Just the os getting the dir that is current at the moment
 execution_path = os.getcwd()
@@ -23,34 +25,61 @@ detector.setModelTypeAsRetinaNet()
 # get the path of .h5 / find location folder that holds resnet50_coco_best_v2.0.1.h5
 
 try:
-    detector.setModelPath( os.path.join(execution_path, "resnet50_coco_best_v2.0.1.h5"))
+    detector.setModelPath(os.path.join(execution_path, "resnet50_coco_best_v2.0.1.h5"))
     detector.loadModel()
     print(yellow + 'Execution Path Completed, can begin to detect image information' + end)
 except OSError:
-    print(bold + red + 'Unable to complete the operation, please check line 26 -> os.path.join for more information' + '\n' +
-          'Thank you.' + end)
+    print(
+        bold + red + 'Unable to complete the operation, please check line 26 -> os.path.join for more information' + '\n' +
+        'Thank you.' + end)
+
+# Get image folder location
+path = 'images'
+# f = files
+for images_folder in os.walk(path):
+    for image_folder in images_folder:
+        if '.jpg' in image_folder:
+            image_folder.append(os.path.join(images_folder, image_folder))
+# Print image info
+print(image_folder)
+
+# Aquire User input on what image they want to scan for data
+print('Key Legend: Image name.jpg or png -> Enter')
+
+image_see = input('Please type in the full name & image format you would like to scan: ')
+
+# User input turned into string
+image_aquire = str(image_see)
+# img completed statement
+img_full_info = '_img_complete'
+img_format = '.jpg'
+# combine image info
+img_complete = image_aquire + img_full_info + img_format
 
 try:
     detections = detector.detectObjectsFromImage(
         # find the img
-
-        input_image=os.path.join(execution_path, "images/cat00.jpg"),
+        input_image=os.path.join(execution_path, "images/" + image_aquire),
         # out put the image / rename it with a new name
-        output_image_path=os.path.join(execution_path, "image_detection_complete/cat_image_detect.jpg"),
+        output_image_path=os.path.join(execution_path, "image_detection_complete/" + img_complete),
         minimum_percentage_probability=50,
     )
+
+
 except OSError:
     print(bold + red + 'Cannot acquire os.path, please check lines 37-39 for any issues' + '\n' +
-          'Thank you.' + end )
+          'Thank you.' + end)
 
 # just loops & tell the user the img name & the % of that img being that obj
 # e.g -> tree 50% , or human 85%, city stop lights 77%, etc
 try:
     for eachObject in detections:
         print(bold + yellow + 'Acquired Image Info Success! Following image info is the following: ' + '\n' + end)
-        print(bold , blue , eachObject["name"], ": ", end, bold, purple, round(eachObject["percentage_probability"]),'%', end)
+        print(bold, blue, eachObject["name"], ": ", end, bold, purple, round(eachObject["percentage_probability"]), '%',
+              end)
+
 
 
 except Exception:
-    print(bold + red + 'Unable to complete the process, please check lines 49-52 for any issues'+ '\n' +
-          'Thank you.' + end )
+    print(bold + red + 'Unable to complete the process, please check lines 49-52 for any issues' + '\n' +
+          'Thank you.' + end)
